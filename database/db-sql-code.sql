b0476382c91e6ba4015884c7d3eacc05bf9572ef
teory01 <youremail@byupathway.edu>
2026-01-30 08:45:02 +0100

    fv

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -241,11 +241,19 @@ VALUES   (
     5
   );
 
+-- 4. Update the GM Hummer description
+-- Replace 'small interiors' with 'a huge interior'
 UPDATE public.inventory
-SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
-WHERE inv_make='GM' and inv_model = 'Hummer';
+SET inv_description = REPLACE(
+  inv_description,
+  'small interiors',
+  'a huge interior'
+)
+WHERE inv_make = 'GM'
+AND inv_model = 'Hummer';
 
+-- 6. Update all image paths to include '/vehicles'
 UPDATE public.inventory
-SET 
+SET
   inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
   inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
