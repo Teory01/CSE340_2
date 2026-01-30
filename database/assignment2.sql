01fd363ea799a25e1caf9dfffc139dca28aa3048
teory01 <youremail@byupathway.edu>
2026-01-30 07:36:43 +0100

    make changes

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,35 +1,59 @@
-INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password)
-VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+-- 1. Insert a new account record for Tony Stark
+INSERT INTO public.account (
+  account_firstname,
+  account_lastname,
+  account_email,
+  account_password
+)
+VALUES (
+  'Tony',
+  'Stark',
+  'tony@starkent.com',
+  'Iam1ronM@n'
+);
 
-
-
+-- 2. Modify Tony Stark's account_type to 'Admin'
+-- Uses the primary key via a subquery
 UPDATE public.account
 SET account_type = 'Admin'
-WHERE account_email = 'tony@starkent.com';
+WHERE account_id = (
+  SELECT account_id
+  FROM account
+  WHERE account_email = 'tony@starkent.com'
+);
 
-
-
+-- 3. Delete the Tony Stark account record
+-- Uses the primary key via a subquery
 DELETE FROM public.account
-WHERE account_email = 'tony@starkent.com';
+WHERE account_id = (
+  SELECT account_id
+  FROM account
+  WHERE account_email = 'tony@starkent.com'
+);
 
-
-
+-- 4. Update the GM Hummer description
+-- Replace 'small interiors' with 'a huge interior'
 UPDATE public.inventory
-SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
-WHERE inv_make='GM' and inv_model = 'Hummer';
+SET inv_description = REPLACE(
+  inv_description,
+  'small interiors',
+  'a huge interior'
+)
+WHERE inv_make = 'GM'
+AND inv_model = 'Hummer';
 
-
-
-SELECT i.inv_make, i.inv_model, c.classification_name
-FROM public.inventory i
-INNER JOIN public.classification c
+-- 5. Select make, model, and classification name for Sport vehicles
+SELECT
+  i.inv_make,
+  i.inv_model,
+  c.classification_name
+FROM inventory i
+INNER JOIN classification c
   ON i.classification_id = c.classification_id
 WHERE c.classification_name = 'Sport';
 
-
-
-
+-- 6. Update all image paths to include '/vehicles'
 UPDATE public.inventory
-SET 
+SET
   inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
   inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
