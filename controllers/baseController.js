f1122815ae895723eff7dc543cab1480d4055c73
teory01 <youremail@byupathway.edu>
2026-02-07 15:41:15 +0100

    fvfv

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,4 +1,4 @@
-const utilities = require("../utilities")
+const utilities = require("../utilities/")
 const baseController = {}
 
 baseController.buildHome = async function(req, res){
@@ -9,15 +9,4 @@ baseController.buildHome = async function(req, res){
   })
 }
 
-<<<<<<< HEAD
-/* *********************************
- * Task 3 Trigger a 500 Server Error
- * ****************************** */
-baseController.triggerError = async function (req, res, next) {
-  throw new Error("500 Server Error")  
-}
-
-
-=======
->>>>>>> c45a88358101a98694d3d61b02612c7228c741c3
 module.exports = baseController
